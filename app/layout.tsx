c1da4c76d3029acca6301c3085b45a84d79a8b8c
Abdulramon Lasisi <lasisiolalekan081@gmail.com>
2023-01-30 12:51:20 +0100

    Added needed dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,16 @@
 import './globals.css'
 
-export default function RootLayout({
-  children,
-}: {
+interface Props {
   children: React.ReactNode
-}) {
+}
+
+const RootLayout = ({ children }: Props) => {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang='en'>
       <head />
       <body>{children}</body>
     </html>
   )
 }
+
+export default RootLayout
